d740d2ab1399c097ff95487fafeede737c0d060b
sishenhei7 <982505136@qq.com>
2020-02-04 16:58:13 +0800

    feat: 提交组件；定下ToDo计划

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # vue-waterfall-nice
 
-> vue 的瀑布流组件，支持无图、多图模式，支持服务端渲染，支持懒加载，使用非常简单！
+> vue 的瀑布流组件，支持无图、多图模式，支持自定义布局，支持服务端渲染，支持懒加载，使用非常简单！
+
+本插件极大地借鉴了 [vue-waterfall-adaptive](https://github.com/geng452654716/vue-waterfall)
+
+## ToDo
+
+- [ ] 完善 vue-waterfall-nice
+- [ ] 用 ts 重写
+- [ ] 用 rollup 打包
+- [ ] 添加 demo
+- [ ] 用 jest 测试
---
+++ lib/WaterfallSlide.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    ref="waterfallSilde"
+    class="waterfall-silde"
+    :style="style"
+    :class="{ 'show': init }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WaterfallSlide',
+  inject: ['parent'],
+  props: {
+    // 需要预加载的图片数组
+    imgs: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      height: 0,
+      top: 0,
+      left: 0,
+      resize: false,
+      init: false,
+    };
+  },
+  computed: {
+    style() {
+      const style = {
+        width: `${this.parent.mySlideWidth}px`,
+      };
+
+      if (this.init) {
+        style.top = `${this.top}px`;
+        style.left = `${this.left}px`;
+      }
+
+      return style;
+    },
+  },
+  mounted() {
+    this.notificationParent();
+  },
+  methods: {
+    // 通知父组件添加子实例
+    notificationParent() {
+      this.parent.allChildren.push(this);
+      this.parent.childrens.push(this);
+    },
+    // 刷新高度
+    getHeight() {
+      return this.imgLoad()
+        .then(() => {
+          this.height = this.$refs.waterfallSilde.offsetHeight;
+        });
+    },
+    // 图片不定高的时候预加载
+    imgLoad() {
+      if (!this.imgs.length) return Promise.resolve('imgLoadJump');
+      return Promise.all(this.imgs.map(item => this.everyImgLoad(item)));
+    },
+    // 单张图片预加载
+    everyImgLoad(imgSrc) {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = imgSrc;
+        img.onload = () => {
+          resolve('imgLoadFinish');
+        };
+        img.onerror = () => {
+          resolve('imgLoadError');
+        };
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.waterfall-silde {
+  position: absolute;
+  transition: left 0.5s, top 0.5s;
+  visibility: hidden;
+}
+.show {
+  visibility: visible;
+  animation: show-card 0.5s;
+}
+@keyframes show-card {
+  0% {
+    transform: translateY(50px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
